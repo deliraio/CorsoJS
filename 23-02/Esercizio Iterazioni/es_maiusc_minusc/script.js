ea0773d2daec6f513ee066de3b77a377b9a647fc
deliraio <100274181+deliraio@users.noreply.github.com>
2022-02-23 21:26:43 +0100

    Risolto esericizio upper_lower non funzionante

--- 23-02/Esercizio Iterazioni/es_maiusc_minusc/script.js
+++ 23-02/Esercizio Iterazioni/es_maiusc_minusc/script.js
@@ -1,42 +1,45 @@
 function maiuscMinusc() {
-    var stringa = "sOtTo LA pANCa la Cpra caNTA"
-    var parola = stringa.split(' ');
-    var stringaFinale = "";
+  var stringa = "sOtTo LA pANCa la CApra caNTA";
+  var parola = stringa.split(" ");
+  var stringaFinale = "";
 
-    console.log(parola);
-    for (let i = 0; i < parola.length; i++) {
-        var ascii = parola[i].charAt(0);
+  console.log(parola);
+  for (var i = 0; i < parola.length; i++) {
+    let ascii = parola[i].charAt(0);
 
-        if (ascii <= 'A' && ascii >= 'Z') {
-            let appo1 = parola[i].charCodeAt(0);
-            appo1 -= 32;
-            stringaFinale += String.fromCharCode(appo1);
-        }
-        if (ascii >= 'A' && ascii <= 'Z') {
-            let appo1 = parola[i].charCodeAt(0);
-            stringaFinale += String.fromCharCode(appo1);
-        }
-        console.log(stringaFinale);
-        for (let j = 1; j < parola.length; j++) {
-            if (parola[i].charAt(j) >= 'A' && parola[i].charAt(j) <= 'Z') {
-                let appo2 = parola[i].charCodeAt(j);
-                appo2 += 32;
-                stringaFinale += String.fromCharCode(appo2);
-            }
-            if (ascii >= 'A' && ascii <= 'Z') {
-                let appo2 = parola[i].charCodeAt(j);
-                stringaFinale += String.fromCharCode(appo2);
-            }
-            console.log(stringaFinale);
-        }
-        stringaFinale += " ";
-
+    if (ascii >= "A" && ascii <= "Z") {
+      //se il codice ascii della prima lettera è compreso tra la MAIUSCOLE
+      let appo1 = parola[i].charCodeAt(0);
+      stringaFinale += String.fromCharCode(appo1);
     }
+    if (ascii >= "a" && ascii <= "z") {
+      //altrimenti se il codice asci della prima lettera è compreso tra le MINUSCOLE
+      let appo1 = parola[i].charCodeAt(0);
+      appo1 -= 32;
+      stringaFinale += String.fromCharCode(appo1);
+    }
+    console.log(parola[i].length);
 
+    for (let j = 1; j < parola[i].length; j++) {
+      let ascii2 = parola[i].charAt(j);
+      //console.log(parola[i].charAt(j));
+      if (ascii2 >= "a" && ascii2 <= "z") {
+        // se è tra le MINUSCOLEB
+        let appo2 = parola[i].charCodeAt(j);
+        stringaFinale += String.fromCharCode(appo2);
+      }
+      if (ascii2 >= "A" && ascii2 <= "Z") {
+        // se è  tra le MAIUSCOLE
+        let appo2 = parola[i].charCodeAt(j);
 
-    console.log(stringaFinale);
+        appo2 += 32;
+        stringaFinale += String.fromCharCode(appo2);
+      }
+    }
+    stringaFinale += " ";
+  }
 
+  console.log(stringaFinale);
 }
-
 
 maiuscMinusc();
