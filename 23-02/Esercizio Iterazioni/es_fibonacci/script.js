11c35f7d8ac8cd1ea07ac1e7c3efb94836ad5b94
deliraio <100274181+deliraio@users.noreply.github.com>
2022-02-23 15:06:58 +0100

    modifica esercizio fibonacci
    aggiunta verifica valore n>=2

--- 23-02/Esercizio Iterazioni/es_fibonacci/script.js
+++ 23-02/Esercizio Iterazioni/es_fibonacci/script.js
@@ -1,15 +1,21 @@
 function fibonacci() {
     var n = document.getElementsByName("num");
     n = n[0].value;
-    var a = 0;
-    var b = 1;
-    var appoggio;
-    console.log(a);
-    console.log(b);
-    for (let i = 0; i < n; i++) {
-        appoggio = a + b;
-        a = b;
-        b = appoggio;
-        console.log(appoggio);
+    if (n >= 2) {
+        var a = 0;
+        var b = 1;
+        var appoggio;
+        console.log(a);
+        console.log(b);
+        for (let i = 0; i < n; i++) {
+            appoggio = a + b;
+            a = b;
+            b = appoggio;
+            console.log(appoggio);
+        }
+        return;
+    } else {
+        alert("hai inserito un numero minore di 2 per la sequenza");
+        return;
     }
 }
